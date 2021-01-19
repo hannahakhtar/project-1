56a7e9b2c51b82b3d8de76e191f52fceb3cb524e
Hannah Akhtar <hannahaakhtar@gmail.com>
2021-01-19 18:32:27 +0000

    bombs and alien complete

--- js/grid.js
+++ js/grid.js
@@ -1,4 +1,5 @@
 
+// variables
 const width = 20
 const cells = []
 let displayAliens = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
@@ -6,16 +7,22 @@ let playerStart = 388
 let direction = 1
 const aliensMoving = false
 let bombMoving = false
-const bombInitiated = false
-// const makeAliensMove = null
+let bombInitiated = false
 let bombStartPosition
 let laserPosition = 388
-const grid = document.querySelector('.grid')
-let displayLives = document.querySelector('#lives_remaining')
-let displayPoints = document.querySelector('#score')
-const startButton = document.querySelector('#start')
-const resetButton = document.querySelector('#reset')
+let scoreUpdate = 0
+let newLaserPos
+// let newerLaserPos
+let gameOver = true
+let laserShot
+let bombOnMove
+let hasLaser = false
 
+// DOM elements
+const grid = document.querySelector('.grid')
+const displayLives = document.querySelector('#lives_remaining')
+const displayPoints = document.querySelector('#score')
+const startButton = document.querySelector('#start')
 
 // create grid
 for (let index = 0; index < width * width; index++) {
@@ -23,7 +30,7 @@ for (let index = 0; index < width * width; index++) {
   cell.classList.add('cell')
   grid.appendChild(cell)
   cells.push(cell)
-  cell.innerHTML = index
+  // cell.innerHTML = index
   cell.style.width = `${100 / width}%`
 }
 
@@ -37,10 +44,18 @@ startButton.addEventListener('click', () => {
     cells[alien].classList.add('alien')
   })
 
-  aliensMove()
-  bombAlignedToAlien()
-  bombsMove()
+  gameOver = false
 
+  checkCollision()
+  bombAlignedToAlien()
+  aliensMove()
+
+  bombOnMove = setInterval(() => {
+    if (bombMoving === true) {
+      moveBomb()
+      // bombOnMove = setInterval(moveBomb, 150)
+    }
+  }, 150)
 
   // when player clicks arrows to move
   document.addEventListener('keydown', (event) => {
@@ -61,34 +76,35 @@ startButton.addEventListener('click', () => {
       cells[playerStart].classList.add('gun')
       cells[laserPosition].classList.add('laser')
     } else if (key === 'ArrowUp') {
-      let newLaserPos = laserPosition - 20
-      setInterval(() => {
-        cells[newLaserPos].classList.remove('laser')
-        newLaserPos -= 20
-        cells[newLaserPos].classList.add('laser')
-      }, 100)
+      // work on this in AM
+      if (hasLaser === false) {
+        hasLaser = true
+        newLaserPos = laserPosition - 20
+        
+        if (newLaserPos > 19) {
+          console.log('yes')
+          laserShot = setInterval(() => {
+            cells[newLaserPos].classList.remove('laser')
+            newLaserPos -= 20
+            cells[newLaserPos].classList.add('laser')
+          }, 100)
+
+        } else if (newLaserPos <= 19) {
+          console.log('hello')
+          clearInterval(laserShot)
+          hasLaser = false
+        } 
+
+        // else {
+        //   laserShot = setInterval(() => {
+        //     cells[newLaserPos].classList.remove('laser')
+        //     newLaserPos -= 20
+        //     cells[newLaserPos].classList.add('laser')
+        //   }, 100)
+        // }
+      }
     }
   })
-
-  // if (displayLives.innerHTML === 0 || displayAliens[] === 399) {
-  //   gameOver()
-  // } else if (displayAliens.length === 0) {
-  //    player wins 
-  // } else if (cells.classList.contains('bomb') && cells.classList.contains('playerStart')) {
-  //   displayLives.innerHTML -= 1
-  // } else if (cells.classList.contains('alien') && cells.classList.contains('laser')) {
-  //   displayPoints.innerHTML += 100
-  // }
-
-  // when user clicks to restart game - needs updating, not currently working
-
-  // resetButton.addEventListener('click', () => {
-
-  //   cells[playerStart].classList.remove('gun')
-  //   displayAliens.forEach(alien => {
-  //     cells[alien].classList.remove('alien')
-  //   })
-  // })
 })
 
 // direction is defined at start of game, so movement will begin by going right
@@ -103,6 +119,9 @@ function alienMovement() {
   } else if (direction === width && displayAliens[0] % width === 0) {
     direction = 1
   }
+  // else if (direction === width && cells[displayAliens] > 380) {
+  //   alert('You Lost')
+  // }
   updateAlienClass()
 }
 
@@ -115,7 +134,7 @@ function removeAlienClass() {
 
 // and then update the new position with updateAlienClass
 function updateAlienClass() {
-  displayAliens = displayAliens.map(alienPosition => alienPosition + direction)
+  displayAliens = displayAliens.map(alien => alien + direction)
   displayAliens.forEach((alien) => {
     cells[alien].classList.add('alien')
   })
@@ -132,37 +151,69 @@ function randomBomb() {
   const alienBomber = Math.floor(Math.random() * displayAliens.length)
   bombStartPosition = displayAliens[alienBomber] + 20
   cells[bombStartPosition].classList.add('bomb')
+  bombMoving = true
+  bombInitiated = true
+  bombBottom = false
 }
 
 function bombAlignedToAlien() {
-  if (bombInitiated === false) {
-    setInterval(randomBomb, 3000)
-  }
+  setInterval(() => {
+    if (bombInitiated === false) {
+      randomBomb()
+    }
+  }, 2500)
 }
 
 function moveBomb() {
-  cells[bombStartPosition].classList.remove('bomb')
-  bombStartPosition += 20
-  cells[bombStartPosition].classList.add('bomb')
-}
-
-function bombsMove() {
-  if (bombMoving === false) {
-    bombMoving = setInterval(moveBomb, 150)
+  if (cells[bombStartPosition]) {
+    cells[bombStartPosition].classList.remove('bomb')
+    bombStartPosition += 20
+    cells[bombStartPosition].classList.add('bomb')
   }
+
 }
 
-function gameOver() {
-  alert(`Game Over! Your final score was ${displayPoints}`)
+let bombBottom = false
+
+function checkCollision() {
+  const collisionCheck = setInterval(() => {
+    if (gameOver === true) {
+      clearInterval(collisionCheck)
+    } else if (cells[newLaserPos]) {
+      
+      // if (cells[0].classList.contains('laser') || cells[1].classList.contains('laser') || cells[2].classList.contains('laser') || cells[4].classList.contains('laser') || cells[5].classList.contains('laser') || cells[0].classList.contains('laser') || )
+
+
+      if (cells[newLaserPos].classList.contains('bomb')) {
+        hasLaser = false
+        console.log('collision')
+        clearInterval(laserShot)
+        bombMoving = false
+        cells[newLaserPos].classList.remove('laser')
+        cells[newLaserPos].classList.remove('bomb')
+        bombInitiated = false
+      }
+      //  another if statement
+      if (cells[newLaserPos].classList.contains('alien')) {
+        hasLaser = false
+        clearInterval(laserShot)
+        const collision = displayAliens.indexOf(newLaserPos)
+        displayAliens.splice(collision, 1)
+        cells[newLaserPos].classList.remove('laser')
+        newLaserPos = 0
+        cells[newLaserPos].classList.remove('alien')
+        displayPoints.innerHTML = scoreUpdate
+        scoreUpdate += 150
+      }
+    } else if (bombStartPosition > 380 && !bombBottom) {
+      bombMoving = false
+      bombInitiated = false
+      console.log(typeof (bombStartPosition))
+      bombBottom = true
+      setTimeout(() => {
+        const bottomRow = [380, 381, 382, 383, 384, 385, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 399]
+        bottomRow.forEach(cell => cells[cell].classList.remove('bomb'))
+      }, 100)
+    }
+  }, 1)
 }
-
-// when player clicks up
-
-
-
-let laserColumn = laserPosition - 20
-let columnArray = []
-for (let j = 0; j < width.length - 1; j++) {
-  columnArray.push(laserColumn - 20)
-}
-fr 
